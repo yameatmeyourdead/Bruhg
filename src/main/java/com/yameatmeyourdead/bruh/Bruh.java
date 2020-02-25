0dd4b27e8bc6b4513985632f95a0eae9c7dfcb53
Yamea <Yamea@192.168.0.2>
2020-02-24 17:52:35 -0700

    yuh oregen

--- src/main/java/com/yameatmeyourdead/bruh/Bruh.java
+++ src/main/java/com/yameatmeyourdead/bruh/Bruh.java
@@ -4,6 +4,7 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 import com.yameatmeyourdead.bruh.init.BlockInit;
+import com.yameatmeyourdead.bruh.world.gen.BruhOreGen;
 
 import net.minecraft.item.ItemGroup;
 import net.minecraft.item.ItemStack;
@@ -11,13 +12,16 @@ import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.Mod.EventBusSubscriber.Bus;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.event.lifecycle.FMLLoadCompleteEvent;
 import net.minecraftforge.fml.event.server.FMLServerStartingEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 
 
 @Mod("bruh")
+@Mod.EventBusSubscriber(modid = Bruh.MOD_ID, bus = Bus.MOD)
 public class Bruh
 {
     public static final Logger LOGGER = LogManager.getLogger();
@@ -49,6 +53,13 @@ public class Bruh
     {
     	
     }
+    
+    @SubscribeEvent
+    public static void loadCompleteEvent(FMLLoadCompleteEvent event)
+    {
+    	BruhOreGen.generateOre();
+    }
+    
     
     public static class BruhItemGroup extends ItemGroup
     {
